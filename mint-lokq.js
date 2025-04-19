1bc4dd3e2e179eedb66b3bc7dc7a9ef58fad2925
edfarialand <edfaria95@gmail.com>
2025-04-19 08:51:35 +0000

    Add LOKQ token minting script

---
+++ mint-lokq.js
@@ -0,0 +1,8 @@
+console.log('Preparing to mint LOKQ token with the following features:');
+console.log('- Name: Lokquidity - mint auth revo - liquidity locked');
+console.log('- Symbol: LOKQ');
+console.log('- Decimals: 9');
+console.log('- Features: Mint authority revoked, locked liquidity');
+console.log('- Wallet: B1ZE8ufwLkQddmxVrfWPVFi96uQRK4rGdYDr9SNKi6TK');
+console.log('\nStarting token creation process...\n');
+require('./solana-token.js');
