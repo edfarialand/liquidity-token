1f933bbede0cd86f900631ff1131eaf48412ea09
edfarialand <105104922+edfarialand@users.noreply.github.com>
2025-05-10 03:32:15 -0400

    Delete set-api-key.sh

--- set-api-key.sh
+++
@@ -1,8 +0,0 @@
-#\!/bin/bash
-# Set the NFT.Storage API key as an environment variable
-
-# Store the API key (replace "PASTE_YOUR_API_KEY_HERE" with your actual API key)
-export NFT_STORAGE_API_KEY="$1"
-
-echo "NFT_STORAGE_API_KEY has been set"
-echo "Now you can run: ./upload-one-command.sh"
